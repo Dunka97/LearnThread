9abbed1f2951051d53fcfd6d2160e1aeb7316945
Dunka97 <1150311882@qq.com>
2019-08-07 10:15:40 +0800

    Initial commit

---
+++ src/com/dunka/chapter01/Test01.java
@@ -0,0 +1,13 @@
+package com.dunka.chapter01;
+
+/**
+ * @Author Dunka
+ * @Description //TODO
+ * @Date 10:14   2019/8/7
+ * @ClassName Test01
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName());
+    }
+}
